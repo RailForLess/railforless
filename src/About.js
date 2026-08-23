5ec09fc68e34355f233f1241bde27043848d6888
Sean Eddy <tikkisean@gmail.com>
2026-08-22 22:58:11 -0700

    Removed mentions of rileynielsen.com

--- src/About.js
+++ src/About.js
@@ -97,15 +97,6 @@ export default function About() {
 							Riley is a software engineer from Minnesota. He enjoys reading,
 							riding his Onewheel, travel, and creating films in his spare time.
 							Trains and using technology to help others are passions of his.
-							Learn more about him and his projects at{" "}
-							<a
-								href="https://rileynielsen.com"
-								rel="noreferrer"
-								target="_blank"
-							>
-								rileynielsen.com
-							</a>
-							.
 						</p>
 					</div>
 				</div>
